f3e250c0e0e131a028b64deebbe88a53ba155bb8
aakashtripathi011 <aakashtripathi914@gmail.com>
2026-06-16 20:33:50 +0530

    Learning operators in cpp

---
+++ 02_Operators.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+int main(){
+    int a=2;
+    int b=5;
+    // operators
+    std::cout<<a+b<<"\n";
+    std::cout<<a-b<<"\n";
+    std::cout<<a*b<<"\n";
+    std::cout<<a/b<<"\n";
+    std::cout<<a%b<<"\n";
+    // bodmas
+    std::cout<<(2+3)*5<<"\n";
+    // comparision operators
+    std::cout<<(2>5)<<"\n";
+    std::cout<<(5>2)<<"\n";
+    std::cout<<(2==5)<<"\n";
+    std::cout<<(2>=2)<<"\n";
+    std::cout<<('a'!='A')<<"\n";
+    return 0;
+}
