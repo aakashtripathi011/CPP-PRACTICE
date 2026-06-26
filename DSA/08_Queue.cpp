36ea330afa4cdd14ae2c053920b433a877f1d9a4
aakashtripathi011 <aakashtripathi914@gmail.com>
2026-06-27 04:27:37 +0530

    cpp: queue traversal and operations

---
+++ DSA/08_Queue.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+ queue<int> q;
+ q.push(10);
+ q.push(20);
+ q.push(30);
+
+cout<<q.front()<<endl;
+q.pop();
+cout<<"\n";
+
+while(!q.empty()){
+    cout<<q.front()<<endl;
+    q.pop();
+}
+
+cout<<"size: "<<q.size();
+
+return 0;
+}
